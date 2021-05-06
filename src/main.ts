c0feb55d8f3ee39e84bb23ec51e8445c30ac28b9
Timo972 <44401485+Timo972@users.noreply.github.com>
2021-05-06 14:57:05 +0200

    get rid of primevue

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,12 @@
 import { createApp } from 'vue'
-import PrimeVue from 'primevue/config'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import lang from './lang'
 
-import InputNumber from 'primevue/inputnumber'
-import Dropdown from 'primevue/dropdown'
-import Button from 'primevue/button'
-
 const app = createApp(App)
-
-app.component('InputNumber', InputNumber)
-app.component('Dropdown', Dropdown)
-app.component('Button', Button)
 
 app.use(store)
     .use(router)
     .use(lang)
-    .use(PrimeVue, { ripple: true })
     .mount('#app')
